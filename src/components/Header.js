a5cdbf2bd60fd9949e1fbcda3f6a0529f6221bda
John Paulo Panganiban <olopangs@gmail.com>
2021-05-17 02:40:52 +0800

    ADD: React Scroll Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,89 @@
-import React from 'react'
-
+import React, { useRef } from 'react'
+import styled from 'styled-components'
+import { Link as LinkScroll } from 'react-scroll'
+import { animateScroll as scroll } from 'react-scroll'
+import { itemMenu } from '../data'
 const Header = () => {
+    const myRef = useRef(null)
+    function scrollToTop() {
+        scroll.scrollToTop();
+    }
     return (
-        <div>
-        </div>
+        <HeaderContainer>
+            <a href="#" onClick={scrollToTop}>
+                <img src="/images/logo.svg" alt="" />
+            </a>
+            <Menu>
+                {itemMenu.map((item) => (
+                    <MenuLink to={item.id}
+                        activeClass="active"
+                        spy={true}
+                        smooth={true}
+                        duration={500}
+                    ><span>{item.title}</span></MenuLink>
+                ))}
+            </Menu>
+            <RightMenu>
+                <a href="#">Shop</a>
+                <a href="#">Tesla Account</a>
+            </RightMenu>
+        </HeaderContainer>
     )
 }
 
 export default Header
+const MenuLink = styled(LinkScroll)`
+cursor: pointer;
+>span {
+    transition: 0.3s ease-in-out;
+    :hover {
+    color: gray;
+    transition: 0.2s ease-in-out;
+}
+
+}
+&.active {
+    border-bottom: 2px solid gray;
+    opacity: 0.6;
+}
+`
+const Links = styled(LinkScroll)`
+
+`
+const RightMenu = styled.div`
+a {
+    font-weight: 600;
+    text-transform: uppercase;
+    /* padding top bottom = 0 */
+    /* padding left right = 10px */
+    padding: 0 10px;
+    /* flex: 1; */
+    justify-content: center;
+    flex-wrap: nowrap;
+}
+`
+const Menu = styled.div`
+display: flex;
+align-items: center;
+a {
+    font-weight: 600;
+    text-transform: uppercase;
+    /* padding top bottom = 0 */
+    /* padding left right = 10px */
+    padding: 0 10px;
+    /* flex: 1; */
+    justify-content: center;
+    flex-wrap: nowrap;
+}
+`
+const HeaderContainer = styled.div`
+top: 0;
+left: 0;
+right: 0;
+min-height: 60px;
+position: fixed;
+display: flex;
+align-items: center;
+justify-content: space-between;
+padding: 0 20px;
+`
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,54 +1,27 @@
 import React from 'react'
 import styled from 'styled-components'
 import Section from './Section'
+import {itemMenu} from '../data'
 const Home = () => {
     return (
         <HomeContainer>
-            <Section
-                title='Model S'
-                description='Order Online for Touchless Delivery'
-                backgroundImg='model-s.jpg'
-                leftBtnText='Custom Order'
-                rightBtnText='Existing Inventory'
-            />
-            <Section
-                title='Model Y'
-                description='Order Online for Touchless Delivery'
-                backgroundImg='model-y.jpg'
-                leftBtnText='Custom Order'
-                rightBtnText='Existing Inventory'
-            />
-            <Section
-                title='Model 3'
-                description='Order Online for Touchless Delivery'
-                backgroundImg='model-3.jpg'
-                leftBtnText='Custom Order'
-                rightBtnText='Existing Inventory'
-            />
-            <Section
-                title='Model X'
-                description='Order Online for Touchless Delivery'
-                backgroundImg='model-x.jpg'
-                leftBtnText='Custom Order'
-                rightBtnText='Existing Inventory' />
-            <Section
-                title='Lowest Cost Solar Panels in America'
-                description='Money-back guarantee'
-                backgroundImg='solar-panel.jpg'
-                leftBtnText='Order now'
-                rightBtnText='Learn more' />
-            <Section
-                title='Solar for New Roofs'
-                description='Money-back guarantee'
-                backgroundImg='solar-roof.jpg'
-                leftBtnText='Order now'
-                rightBtnText='Learn more' />
+            {itemMenu.map((item) => (
+            <div id={item.id}>
+                <Section
+                    title={item.title}
+                    description={item.description}
+                    backgroundImg={item.image}
+                    leftBtnText='Custom Order'
+                    rightBtnText='Existing Inventory'
+                />
+            </div>
+            ))}
             <Section
                 title='Accessories'
                 description=''
                 backgroundImg='accessories.jpg'
                 leftBtnText='Shop now'
-                downArrowHidden={true}/>
+                downArrowHidden={true} />
 
         </HomeContainer>
     )
